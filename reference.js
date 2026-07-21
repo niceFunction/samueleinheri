9addcbab32a5e4144f01393f72becc1b4e1174ad
Samuel Einheri <Samuel.einheri@gmail.com>
2026-07-21 18:32:39 +0200

    added a reference js file

---
+++ reference.js
@@ -0,0 +1,8 @@
+const SITE_REFERENCES = {
+    mail:
+        "mailto:insertmailhere",
+    linkedin:
+        "https://www.linkedin.com/in/your-linkedin-name/",
+    resume:
+        "./DOWNLOAD/SamuelEinheri_Resume.pdf"
+};

026d62058b3818f81b8773a2b46138e31a9f02de
Samuel Einheri <Samuel.einheri@gmail.com>
2026-07-22 19:10:12 +0200

    Hopefully resolved some bugs

--- reference.js
+++ reference.js
@@ -1,8 +1,17 @@
 const SITE_REFERENCES = {
+    
+    /* ICONS */
     mail:
         "mailto:samuel.einheri@gmail.com",
     linkedin:
         "https://www.linkedin.com/in/samueleinheri/",
     resume:
-        "./DOWNLOAD/SamuelEinheri_Resume.pdf"
+        "./DOWNLOAD/SamuelEinheri_Resume.pdf",
+
+    /* PAGES */
+    portfolio: "./portfolio.html",
+
+    prototypes: "./prototypes.html",
+
+    about: "./about.html"
 };

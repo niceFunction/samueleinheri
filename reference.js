b4349f3c2c60b62ac6a7f3dcca59b309624584e9
Samuel Einheri <Samuel.einheri@gmail.com>
2026-08-08 12:24:05 +0200

    reverting stuff

--- reference.js
+++ reference.js
@@ -21,58 +21,6 @@ const SITE_REFERENCES = {
         "./about-me"
 };
 
-const PROJECT_REFERENCES = {
-
-    superPaparazziPage:
-        "./Super-Paparazzi/",
-
-    GriefPage:
-        "./Grief/",
-
-    TectonicusPage:
-        "./Tectonicus/",
-
-    WellDeathHappensPage:
-        "./Well-Death-Happens/",
-
-    MerchantsRushPage:
-        "./Merchants-Rush/",
-
-    DreamWalkerPage:
-        "./DreamWalker/",
-
-    FollowThePathPage:
-        "./FollowThePath/",
-
-    BEPPUSPage:
-        "./BEPPUS/",
-
-     superPaparazziExternal:
-         "https://store.steampowered.com/app/3746050/Super_Paparazzi/",
-
-     GriefExternal:
-         "https://bgibson11.itch.io/grief-overkoalafied",
-
-     TectonicusExternal:
-         "https://gamucatex.itch.io/tectonicus",
-
-     WellDeathHappensExternal:
-         "https://functionsam.itch.io/well-death-happens",
-
-    MerchantsRushExternal:
-         "https://store.steampowered.com/app/2110600/Merchants_Rush/",
-
-    DreamWalkerExternal:
-         "https://simonolivecrona.itch.io/dreamwalker",
-
-    FollowThePathExternal:
-         "https://github.com/niceFunction/followThePath/tree/master/follow_The_Path/followThePath/Assets",
-         
-    BEPPUSExternal:
-         "https://futuregames.itch.io/fg20-ft-gp4-beppus"
-
-};
-
 const SITE_SETTINGS = {
 
     pageBackground: "#2D3436"

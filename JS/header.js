5d5b17d630d23ee3a6abfe895b60474a8666a547
Samuel Einheri <Samuel.einheri@gmail.com>
2026-08-14 09:35:08 +0200

    Doing a test on the header html

--- JS/header.js
+++ JS/header.js
@@ -1,20 +1,33 @@
 fetch("./header.html")
 
 .then(response => {
+
     if (!response.ok) {
+
         throw new Error("Could not load header.html");
+
     }
+
     return response.text();
+
 })
 
 .then(data => {
 
+    /* START: INSERT HEADER HTML */
     document.getElementById("header-placeholder").innerHTML = data;
+    /* END: INSERT HEADER HTML */
 
+    /* START: HEADER PROFILE BUTTON */
     document.getElementById("header-profile").onclick = function () {
-        window.location.href = SITE_REFERENCES.portfolio;
-    };
 
+        window.location.href =
+        SITE_REFERENCES.portfolio;
+
+    };
+    /* END: HEADER PROFILE BUTTON */
+
+    /* START: HEADER TEXT */
     document.getElementById("header-portfolio-button").textContent =
     "PORTFOLIO";
 
@@ -23,48 +36,78 @@ fetch("./header.html")
 
     document.getElementById("header-about-button").textContent =
     "ABOUT ME";
+    /* END: HEADER TEXT */
 
-    /* PAGE BUTTON LINKS */
+    /* START: HEADER PAGE BUTTON LINKS */
     /* PORTFOLIO */
     document.getElementById("header-portfolio-button").onclick = function () {
 
-        window.location.href = SITE_REFERENCES.portfolio;
+        window.location.href =
+        SITE_REFERENCES.portfolio;
 
     };
 
     /* PROTOTYPES */
     document.getElementById("header-prototypes-button").onclick = function () {
 
-        window.location.href = SITE_REFERENCES.prototypes;
+        window.location.href =
+        SITE_REFERENCES.prototypes;
 
     };
 
     /* ABOUT ME */
     document.getElementById("header-about-button").onclick = function () {
 
-        window.location.href = SITE_REFERENCES.about;
+        window.location.href =
+        SITE_REFERENCES.about;
 
     };
+    /* END: HEADER PAGE BUTTON LINKS */
 
+    /* START: HEADER ICON BUTTONS */
+    /* MAIL */
     document.getElementById("header-mail-button").onclick = function(){
-        window.location.href = SITE_REFERENCES.mail;
+
+        window.location.href =
+        SITE_REFERENCES.mail;
+
     };
 
+    /* LINKEDIN */
     document.getElementById("header-linkedin-button").onclick = function(){
+
         window.open(
             SITE_REFERENCES.linkedin,
             "_blank"
         );
+
     };
 
+    /* RESUME */
     document.getElementById("header-resume-button").onclick = function(){
+
         window.open(
             SITE_REFERENCES.resume,
             "_blank"
         );
+
     };
+    /* END: HEADER ICON BUTTONS */
+
+    /* START: SHOW HEADER */
+    document
+        .getElementById("header-placeholder")
+        .classList.add("header-loaded");
+    /* END: SHOW HEADER */
+
 })
 
 .catch(error => {
+
     console.error(error);
+
+    document
+        .getElementById("header-placeholder")
+        .classList.add("header-error");
+
 });
